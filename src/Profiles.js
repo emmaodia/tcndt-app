77c52710df8081ac40ff43c205aabc2a9bf33837
emmaodia <odia.emma@gmail.com>
2020-04-05 14:10:37 +0100

    Commented out image to fix errors in Code.

--- src/Profiles.js
+++ src/Profiles.js
@@ -1,17 +1,17 @@
 import React from 'react';
 import profileCard from './profile-card.module.css'
-import {Image, Transformation} from 'cloudinary-react';
+//import {Image, Transformation} from 'cloudinary-react';
 
 const Profiles = ({firstName, lastName, specialty, experience, cv, workAt, email}) => {
     return(
         // https://res.cloudinary.com/emmaodia/image/upload/v1571423773/emmaodia.jpg
         <div className={profileCard.card}>
-            <div className={profileCard.image}>
+            {/* <div className={profileCard.image}>
                 <Image cloudName="emmaodia" publicId="emmaodia.jpg" secure="true">
                     <Transformation width="200" height="250" gravity="face" radius="5" crop="thumb" />
                 </Image>
             </div>
-        
+         */}
 
             <div className={profileCard.prof}>
                 <h2>{firstName} {lastName}</h2>
